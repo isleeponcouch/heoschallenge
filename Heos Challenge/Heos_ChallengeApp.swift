d57e290f5139f78b0de8b8a8181084fa3481b2cd
Tobias Gundry <tobygundry@gmail.com>
2024-11-08 17:55:00 +1000

    Initial commit at point of new project

---
+++ Heos Challenge/Heos_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Heos_ChallengeApp.swift
+//  Heos Challenge
+//
+//  Created by Tobi Gundry on 8/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct Heos_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
